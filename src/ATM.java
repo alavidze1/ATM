e6afc41b5ed690dbd9a4af88ef77cf2ae00028f4
sandroalavidze <sample@bths.edu>
2024-01-18 23:32:44 -0500

    Expansion of ATM, Customer, Account. Need to finish withdraw men option and implement other options as well as transaction history.

--- src/ATM.java
+++ src/ATM.java
@@ -1,2 +1,49 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class ATM {
+    Scanner in = new Scanner(System.in);
+    public ATM(){
+
+    }
+
+    public void start() {
+        System.out.println("Welcome to our ATM system!");
+        System.out.println("Please enter your name to create a new account");
+        String name = in.nextLine();
+        System.out.println("Please enter a PIN security code. A 4 digit integer. Other values may cause error.");
+        int pin = in.nextInt();
+        in.nextLine();
+        Customer customer = new Customer(name, pin);
+        System.out.println("Now that you're registered, lets get you a Savings account and a Checking account.");
+        Account savings = new Account("Savings", 0);
+        Account checking = new Account("Checking", 0);
+        savings.printAccountInfo();
+        checking.printAccountInfo();
+        System.out.println("There we go. Now you can access the true functions of the ATM.");
+        int menuOption = 0;
+        while(menuOption!=7){
+            System.out.println("Enter the number corresponding to which operation you would like to do:\n1. Withdraw money\n" +
+                    "2. Deposit money\n" +
+                    "3. Transfer money between accounts\n" +
+                    "4. Get account balances\n" +
+                    "5. Get transaction history\n" +
+                    "6. Change PIN\n" +
+                    "7. Exit\n");
+            menuOption = in.nextInt();
+            in.nextLine();
+            if(menuOption==1){
+                System.out.println("Would you like to withdraw from your checking or savings account? Type 1 for savings and 2 for checking");
+                int withdrawAccount = in.nextInt();
+                in.nextLine();
+                int withdrawAmount = -1;
+
+                System.out.println("How much money would you like to deposit?");
+                if(withdrawAccount==1){
+
+                }
+            }
+        }
+    }
+
 }
--- src/Account.java
+++ src/Account.java
@@ -1,9 +1,7 @@
 public class Account {
-    private Customer owner;
     private String accountName;
     private double currentBalance;
-    public Account(Customer owner, String accountName, double currentBalance){
-        this.owner = owner;
+    public Account(String accountName, double currentBalance){
         this.accountName = accountName;
         this.currentBalance = currentBalance;
     }
@@ -13,4 +11,9 @@ public class Account {
     public void updateBalance(double balChange){
         currentBalance+=balChange;
     }
+    public void printAccountInfo(){
+        System.out.println(accountName + "Balance: $" + currentBalance);
+    }
+    public void increaseBalance(double amount){currentBalance+=amount;}
+    public void decreaseBalance(double amount){currentBalance-=amount;}
 }
